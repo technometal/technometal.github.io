2013ac92163596317c70cef7d1180bac8cb35207
Heriberto <hrhproducciones@gmail.com>
2021-06-29 14:43:17 +0200

    build files incorporated

---
+++ precache-manifest.a459dff98512fcf36564caf766fb166b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493f7eb13ebce7c1c5eabab9732cd5d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb828fa7d82d50f06585",
+    "url": "/static/css/main.76a02b8b.chunk.css"
+  },
+  {
+    "revision": "acdb62f6ffeffde7f162",
+    "url": "/static/js/2.9147c772.chunk.js"
+  },
+  {
+    "revision": "8fc9e4c131f28ea99c1fba1405458c02",
+    "url": "/static/js/2.9147c772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb828fa7d82d50f06585",
+    "url": "/static/js/main.dca961cc.chunk.js"
+  },
+  {
+    "revision": "a43e3036b6964e71c83a",
+    "url": "/static/js/runtime-main.4ea2608f.js"
+  }
+]);
